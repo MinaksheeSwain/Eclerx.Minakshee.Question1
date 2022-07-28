15d80139c167d144d368d23ff7f86e017f1c179a
Minakshee.Swain <Minakshee.Swain@ECXLC5503>
2022-07-28 17:14:32 +0530

    Add project files.

---
+++ Controllers/RegistrationsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Eclerx.Minakshee.Question1.Models;
+
+namespace Eclerx.Minakshee.Question1.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        public string value = "";
+        // GET: Registrations
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Registration r)
+        {
+            if (Request.HttpMethod == "Post")
+            {
+                Registration rs = new Registration();
+                using (SqlConnection con = new SqlConnection("Data Source=ECXLC5503SQLEXPRESS01;Integrated Security=true;Initial Catalog=hr"))
+                {
+                    using (SqlCommand cmd = new SqlCommand("usp_AddNewStudents", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@StudentId", r.StudentId);
+                        cmd.Parameters.AddWithValue("@UserId", r.UserId);
+                        cmd.Parameters.AddWithValue("@Passwords", r.PassWords);
+                        cmd.Parameters.AddWithValue("@DateOfBirth", r.DateOfBirth.ToString());
+                        cmd.Parameters.AddWithValue("@MobileNo", r.MobileNo.ToString());
+                        cmd.Parameters.AddWithValue("@CreatedDate", r.CreatedDate.ToString());
+                        con.Open();
+                        ViewData["result"] = cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+            }
+            return View();
+        }
+    
+
+
+        }
+    
+}
---
+++ Models/Registration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Eclerx.Minakshee.Question1.Models
+{
+    public class Registration
+    {
+        [Display(Name = "StudentId")]
+        public int StudentId { get; set; }
+        [Required(ErrorMessage = "please enter UserId")]
+        [Display(Name = "UserId")]
+        public string UserId { get; set; }
+        [Display(Name = "Passwords")]
+        public string PassWords { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string MobileNo { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
